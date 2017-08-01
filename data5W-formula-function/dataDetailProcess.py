de0aa202f489946bf45940985c06d6f9f18e84a8
Sy <370418846@qq.com>
2017-08-01 17:30:41 +0800

    2017-8-1 发现实验数据集问题，改正：从中华中医网爬取得5W数据进行实验

---
+++ data5W-formula-function/dataDetailProcess.py
@@ -0,0 +1,421 @@
+# coding=utf-8
+import re
+import data_process
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#****************************输入：compostion_1.csv;输出：composition_1_1.csv***********************************
+#把compostion_1.csv里的数据取出来，按空格分割提取每个成分；
+#注意！这里处理后有的数据会出现多余的空格！***************手动处理那些多余的空格。。。**************************
+#eg.地骨皮30g  防风30g  甘草15g(微炙) =============> 地骨皮30g,防风30g,甘草15g(微炙)
+def composition_process(readcsvname,writecsvname):
+    print ('composition_process')
+    # readcsvname='composition_6.csv'
+    csvdata=data_process.read_csv(readcsvname)
+    datas=[]
+    pattern = re.compile(ur'(?:、|，|；|。|,)')
+    pattern1=re.compile(ur'(?:\(|（)')
+    pattern2 = re.compile(ur'(?:\)|）)')
+    for item in csvdata:
+        item[0]=item[0].decode('utf-8')
+        item[0] = re.sub(pattern, ' ', item[0])
+        item[0] = re.sub(pattern1, ' (', item[0])
+        item[0] = re.sub(pattern2, ') ', item[0])
+
+        item=item[0].split(' ')
+        num = 0
+        for itemdata in item:
+            if itemdata == '':
+                item.pop(num)
+            num += 1
+        datas.append(item)
+    data_process.write_in_csv(writecsvname,datas)
+
+#自动去除上述方法处理后还存在的空格
+def process_blank(readcsvname,writecsvname):
+    print ('process_blank')
+    csvdata=data_process.read_csv(readcsvname)
+    datas=[]
+    i=1
+    replace=[]
+    for item in csvdata:
+        num = 0
+        lengh=len(item)
+        for itemdata in item:
+            # print 'itemdata',i,itemdata
+            if itemdata == '':
+                item.pop(num)
+            itemdata = itemdata.split('\xe3\x80\x80')
+            if len(itemdata)>1:
+                # print 'split itemdata',i,itemdata
+                for zz in itemdata:
+                    replace.append(zz)
+            else:
+                replace.append(itemdata[0])
+            num += 1
+        datas.append(replace)
+        replace=[]
+        i+=1
+    data_process.write_in_csv(writecsvname,datas)
+
+#****************************输入：compostion_1_1.csv;输出：composition_1_2.csv***********************************
+#清除描述中的 “各”字；清除药物括号里的内容
+#eg.苍术(泔浸，去皮净)120g,防风6g,白术,白茯苓,白芍药各3g ======> 苍术120g,防风6g,白术,白茯苓,白芍药#3g
+def splitnumandstr(readcsvname):
+    print ('splitnumandstr')
+    csvdata=data_process.read_csv(readcsvname)
+
+    pattern1=re.compile(ur'(?:\(|（)')
+    pattern2 = re.compile(ur'(?:\)|）)')
+
+    data_after=[]
+    datalist=[]
+    for item in csvdata:
+        # print ('%%%%%%%%%%%%%num',i)
+        check=True
+        for itemdata in item:
+            itemdata=itemdata.replace('﻿','')
+            itemdata = itemdata.decode('utf-8')
+
+            #清洗数据：处理描述中的里括号里的内容***不要括号里的内容**********
+            try:
+                num1,num2 = kuohaoClear(itemdata)
+                itemdata = itemdata[0:num1]+itemdata[num2 :]
+            except:
+                # print 'kuohao error',item[0]
+                if pattern1.search(itemdata):
+                    itemdata = itemdata[:pattern1.search(itemdata).start()]
+                    data_after.append(itemdata)
+                    check=False
+                if pattern2.search(itemdata):
+                    itemdata = itemdata[pattern2.search(itemdata).end():]
+                    check=True
+            if check:
+                data_after.append(itemdata)
+        datalist.append(data_after)
+        data_after=[]
+    return datalist
+
+#****************************输入：compostion_1_1.csv;输出：composition_1_3.csv***********************************
+#提取描述中的药物单位量以及它所对应的药
+#eg1.苍术120g,防风6g,白术,白茯苓,白芍药#3g =================》苍术,120g,防风,6g,白术,3g ,白茯苓,3g ,白芍药,3g
+#eg2.黄芩,防风#等分=================》黄芩,None,防风,None
+def extractnumwithstr(readcsvname):
+    print ('extractnumwithstr')
+    # readcsvname='csvtest_1.csv'
+    # readcsvname='composition_5_2.csv'
+    csvdata=data_process.read_csv(readcsvname)
+
+    # i:指示第i个处方
+    i=1
+    #正则匹配要用' ur'' '才能正确匹配中文
+    #(?:..):(...)的不分组版本，用于使用| 或 后接数量词
+    pattern1 = re.compile(ur'\d+.\d+(?:g|kg|ml|l|个|钱|片|根|条|份|张|枚|具|朵|只|粒|茎|两|斤|挺|对|头|L|ML|分|节|cm|握|株|两半)')
+    pattern2=re.compile(ur'\d+(?:g|kg|ml|l|个|钱|片|根|条|份|张|枚|具|朵|只|粒|茎|两|斤|挺|对|头|L|ML|分|节|cm|握|株|两半)')
+    finalmedicallist=[]
+    for item in csvdata:
+        print '****************************************************************** 处方： ',i
+        medicallist = []
+        point = []
+        medicaldict=[]
+        for itemdata in item:
+            weight = ''
+            yaowulist=[]
+            itemdata = itemdata.replace('﻿', '')
+            itemdata = itemdata.replace('．', '.')
+            itemdata = itemdata.replace('o', '0')
+            itemdata = itemdata.decode('utf8')
+            # print 'itemdata', itemdata
+
+            #在处方内容中通过正则匹配找出数量单位 start
+            weight1=pattern1.findall(itemdata)
+            weight2=pattern2.findall(itemdata)
+            #把正确的值放在变量weight中
+            if(weight1):
+                weight=weight1[0]
+                yaowulist=pattern1.split(itemdata)
+            elif(weight2):
+                weight = weight2[0]
+                yaowulist=pattern2.split(itemdata)
+            # print '$$$$$$$$$$$$findal',weight1,weight2,weight
+            # 在处方内容中通过正则匹配找出数量单位 end
+            # print 'yaowulist',yaowulist
+            # for ii in yaowulist:
+            #     print ii
+
+            # 把处方的每味药提出来重新放在medicallist列表元素[0]里，同时已经去除了药的数量单位
+            if(yaowulist):
+                try:
+                    yaowulist.remove('')
+                    for zz in yaowulist:
+                        medicallist.append(zz)
+                except:
+                    pass
+            else:
+                medicallist.append(itemdata)
+
+            # 把处方的每味药所对应的数量单位存入medicallist 列表元素[1]的位置列表里
+            if(weight):
+                medicallist.append(weight)
+            else:
+                medicallist.append('None')
+
+            #medicallist eg.[u'\u9632\u98ce', u'3l']
+            medicaldict.append(medicallist)
+            medicallist = []
+
+        # print "medicallist", medicallist
+
+        print 'medicaldict',medicaldict
+        #j统计每个处方里的第j味药
+        j=0
+        for k,v in medicaldict:
+            #用point记录#在哪味药上
+            # print k,v
+            if(k.find('各')>0):
+                point.append(j)
+                medicaldict[j][0]=medicaldict[j][0].replace('各','')
+                # print (medicaldict[j][0])
+                print '检测到“各”字，该味药在处方中所处位置：',j,k
+            j+=1
+        print '该方剂一共配药数量为：',j
+        print '该方剂中出现“各”字的位置有：', point
+
+        f=0
+        print '##################### 开始处理所有药的数量单位 ##################'
+        # print ('medicaldict', medicaldict)
+
+
+        for m,n in medicaldict:
+            if(point!=[]):
+                for pointnum in point:
+                    # print 'test', pointnum
+                    if(f>pointnum):
+                        continue
+                    elif (n=='None'):
+                            # print 'test4',medicaldict[f][1],pointnum
+                            medicaldict[f][1]=medicaldict[pointnum][1]
+                            break
+            f+=1
+        # print '@@@@@@~~最后处理结果(列表):', medicaldict
+
+        #重新整理medicaldict数据格式，并存入csv里
+        onepiece=[]
+        for x,y in medicaldict:
+            print x,y
+            onepiece.append(x)
+            onepiece.append(y)
+            # print 'onepiece',onepiece
+
+        # data_process.createListCSV('csvtest_2.csv',medicaldict)
+        finalmedicallist.append(onepiece)
+        #处方数增一
+        i += 1
+
+
+    return finalmedicallist
+    # print (finalmedicallist)
+    # writecsvname = 'composition_2_3.csv'
+    # data_process.createListCSV(writecsvname, finalmedicallist)
+
+
+# ****************************输入：compostion_1_3.csv ~ compostion_6_3.csv; 输出：allData.csv***********************************
+#把六个类的数据全部放在一个csv里:allData.csv
+#eg.*1*4,地骨皮,30g,防风,30g,炙甘草,7.5g
+def createAllList():
+    print ('createAllList')
+    addlist = []
+    for inum in range(1,7):
+        print ('inum',inum)
+        readcsvname='composition_'+str(inum)+'_3.csv'
+        # readcsvname = 'csvtest_2.csv'
+        csvdata = data_process.read_csv(readcsvname)
+        pnum='*'+str(inum)
+        # print 'csvdata',csvdata
+        i=1
+        for item in csvdata:
+            pnum=pnum+'*'+str(i)
+            item.insert(0,pnum)
+            # print 'zzzz:',item
+            addlist.append(item)
+            pnum='*'+str(inum)
+            i+=1
+    # writecsvname = 'allData.csv'
+    #测试
+    # writecsvname = 'csvtest_3.csv'
+    # data_process.write_in_csv(writecsvname, addlist)
+
+
+# **************************** 输入：allData.csv; 输出：allData_normal.csv    ,   allData_none.csv ***********************************
+#把带None的数据和完整数据分开
+def seperateNone():
+    print ('seperateNone')
+    readcsvname = 'csvtest_3.csv'
+    # readcsvname ='allData_none.csv'
+    csvdata = data_process.read_csv(readcsvname)
+    normalList=[]
+    noneLise=[]
+    nn=1
+    #统计每类数据里带NONE的方剂有多少条
+    count=1
+    for item in csvdata:
+        # print 'item',item
+        check=1
+        for itemdata in item:
+            # print 'itemdata',itemdata
+            if(itemdata=='None'):
+                check=0
+                noneLise.append(item)
+                break
+        if(check==1):
+            normalList.append(item)
+        zz=item[0].split('*')
+        # print 'zz',zz
+        if(zz[1]==nn):
+           count+=1
+        else:
+            print ('count :',count)
+            count=1
+        #nn用于判断类别有没有改变
+        nn = zz[1]
+
+        #看最后一类数据时打开
+        # print 'count??? :', count
+
+    # writenormalcsvname = 'csvtest_normal.csv'
+    # writenonecsvname = 'csvtest_none.csv'
+
+    #把数据中带none和完善的数据分别保存在不同的csv里
+    # writenormalcsvname = 'allData_normal.csv'
+    # writenonecsvname = 'allData_none.csv'
+    # data_process.write_in_csv(writenormalcsvname , normalList)
+    # data_process.write_in_csv(writenonecsvname, noneLise)
+
+
+# **************************** 输入：allData_normal.csv (只有normal里面的才带完整的数量单位); 输出：csvtest_normal1.csv ***********************************
+#单位转换为g的函数 加在这里！！
+#把单位kg,钱，两 统一为 g（克）
+def unitTransformation(readcsvname):
+    print ('unitTransformation()')
+    # readcsvname = 'csvtest_normal.csv'
+    # readcsvname = 'allData_normal.csv'
+    # readcsvname ='allData_none.csv'
+    csvdata = data_process.read_csv(readcsvname)
+    normalList=[]
+    for item in csvdata:
+        # print 'item',item
+        midList = []
+        for itemdata in item:
+            # print 'itemdata', itemdata
+            itemdata=itemdata.decode('utf8')
+            if (itemdata.find('两') > 0):
+                try:
+                    zz = itemdata.split('两')
+                    # print 'split itemdata', itemdata
+                    unit = float(zz[0]) * 50
+                    # print 'unit', unit
+                    changeunit = str(unit) + 'g'
+                    # print 'changeunit', changeunit
+                    midList.append(changeunit)
+                except:midList.append(itemdata)
+            elif(itemdata.find('钱')>0):
+                try:
+                    zz = itemdata.split('钱')
+                    unit=float(zz[0])*3.125
+                    # print 'unit',unit
+                    changeunit=str(unit)+'g'
+                    midList.append(changeunit)
+                except:
+                    midList.append(itemdata)
+            elif(itemdata.find('kg')>0):
+                try:
+                    zz = itemdata.split('kg')
+                    unit=float(zz[0])*1000
+                    changeunit=str(unit)+'g'
+                    midList.append(changeunit)
+                except:
+                    midList.append(itemdata)
+            elif (itemdata.find('斤') > 0):
+                try:
+                    zz = itemdata.split('斤')
+                    unit = float(zz[0]) * 500
+                    changeunit = str(unit) + 'g'
+                    midList.append(changeunit)
+                except:
+                    midList.append(itemdata)
+            elif (itemdata.find('分') > 0):
+                try:
+                    zz = itemdata.split('斤')
+                    unit = float(zz[0]) * 0.3
+                    changeunit = str(unit) + 'g'
+                    midList.append(changeunit)
+                except:
+                    midList.append(itemdata)
+            else:
+                midList.append(itemdata)
+        normalList.append(midList)
+
+
+    # writecsvname = 'csvtest_normal1.csv'
+    # writecsvname = 'allData_normal1.csv'
+    # data_process.write_in_csv(writecsvname , normalList)
+    return normalList
+
+# **************************** 输入：allData_none.csv (只有normal里面的才带完整的数量单位); 输出：csvtest_none1.csv ***********************************
+#单位转换为g的函数 加在这里！！
+#把单位kg,钱，两 统一为 g（克）
+def noneStandard(readcsvname):
+    print ('noneStandard')
+    # readcsvname ='allData_none.csv'
+    csvdata =data_process.read_csv(readcsvname)
+
+    noneList = []
+    for item in csvdata:
+        midList = []
+        for itemdata in item:
+            itemdata = itemdata.replace('﻿', '')
+            itemdata = itemdata.replace('等分', '')
+            itemdata = itemdata.replace('少许', '')
+            itemdata = itemdata.replace('3倍于上药', '')
+            itemdata = itemdata.replace('减半', '')
+            itemdata = itemdata.replace('倍加', '')
+            midList.append(itemdata)
+        noneList.append(midList)
+
+    # writecsvname = 'allData_none1.csv'
+    # data_process.write_in_csv(writecsvname , noneList)
+    return noneList
+
+
+#清除方剂里的括号里的补充说明
+def kuohaoClear(str):
+    # print 'kuohaoClear'
+    pattern1=re.compile(ur'(?:\(|（)')
+    pattern2 = re.compile(ur'(?:\)|）)')
+    num1=pattern1.search(str).start()
+    num2=pattern2.search(str).end()
+    # print '1,2',num1,num2
+    return num1,num2
+
+
+if __name__ == '__main__':
+    print ('main')
+    # composition_pracess()
+    # splitnumandstr()
+    # readcsvname = 'composition_2_2.csv'
+    # writecsvname = 'composition_2_3.csv'
+    # extractnumwithstr(readcsvname, writecsvname)
+    # createAllList()
+    # seperateNone()
+    # unitTransformation()
+    # noneStandard()
+    #
+    # str='郁李仁（去皮研为膏）15克'.decode('utf-8')
+    # str1 = '郁李仁（15克)'.decode('utf-8')
+    # try:
+    #     kuohaoClear(str1)
+    # except:
+    #     print 'no'
+
---
+++ data5W-formula-function/dataFrom5w.py
@@ -0,0 +1,201 @@
+# coding=utf-8
+import data_process
+import dataDetailProcess
+import sys
+import re
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def pickData(csv_name,csvname1,csvname2):
+    print 'pickData'
+    preslist=[]
+    funclist=[]
+    write_csv_name=''
+    #取出功能主治
+    funcContent=data_process.extract_ingredients(csv_name,write_csv_name,col_index=4)
+    presContent = data_process.extract_ingredients(csv_name, write_csv_name, col_index=2)
+    num=0
+    for item in funcContent:
+        bianhao='#'+str(num)+'# '
+        funclist.append(bianhao+item)
+        num+=1
+    print '功效个数:',len(funclist)
+    num=0
+    for item in presContent:
+        bianhao='#'+str(num)+'# '
+        preslist.append(bianhao+item)
+        num+=1
+    print '方剂个数:', len(preslist)
+
+    data_process.write_list_in_csv(csvname1,preslist)
+    data_process.write_list_in_csv(csvname2, funclist)
+
+def extractnumfromstr(readcsvname,writecsvname):
+    print ('extractnumwithstr')
+    csvdata = data_process.read_csv(readcsvname)
+
+    # 正则匹配要用' ur'' '才能正确匹配中文
+    # (?:..):(...)的不分组版本，用于使用| 或 后接数量词
+    pattern1 = re.compile(ur'\d+.\d+(?:g|kg|ml|l|千克|克|钱半|斤半|分半|升半|升|个|钱|片|根|条|份|张|枚|具|朵|只|粒|茎|两半|斤|文|挺|对|头|L|ML|分|节|cm|握|株|两|铢)')
+    pattern2 = re.compile(ur'\d+(?:g|kg|ml|l|千克|克|钱半|分半|斤半|升半|升|个|钱|片|根|条|份|张|枚|具|朵|只|合|粒|茎|两半|斤|文|挺|对|头|L|ML|分|节|cm|握|株|两|铢)')
+    pattern3 = re.compile(
+        ur'\d+(?:g|kg|ml|l|千克|克|钱半|分半|斤半|升半|升|个|钱|片|根|条|份|张|枚|具|朵|只|粒|茎|两半|斤|文|挺|合|对|头|L|ML|分|节|cm|握|株|两|铢)\d+(?:g|kg|ml|l|克|钱半|分半|斤半|升半|升|个|钱|片|根|条|份|张|枚|具|朵|只|粒|茎|两半|斤|文|挺|对|头|L|ML|分|节|cm|握|株|两|铢)')
+    pattern4 = re.compile(
+        ur'(?:一|二|三|四|五|六|七|八|九|十|两|半)(?:g|kg|ml|l|千克|克|钱半|斤半|分半|升半|升|个|钱|片|根|条|份|合|张|枚|具|朵|只|粒|茎|两半|斤|文|挺|对|头|L|ML|分|节|cm|握|株|两|铢)')
+
+    pattern_other=re.compile(ur'(?:等分|适量|少许)')
+    medicallist = []
+    for item in csvdata:
+        print '****************************************************************** 处方： ', item[0]
+        checkBH = 0
+        medical = []
+        for itemdata in item:
+            check_ge=False
+            weight = 'None'
+            if checkBH:
+                itemdata = itemdata.replace('﻿', '')
+                itemdata = itemdata.replace('．', '.')
+                itemdata = itemdata.replace('o', '0')
+                itemdata = itemdata.decode('utf8')
+                # print 'itemdata', itemdata
+                if itemdata.find('各')>-1:
+                    check_ge=True
+                itemdata = itemdata.replace('各', '')
+                itemdata = re.sub(pattern_other, '', itemdata)
+                match1 = pattern3.search(itemdata)#组合，1两2钱
+                match2 = pattern1.search(itemdata)#小数
+                match3 = pattern2.search(itemdata)#整数
+                match4 = pattern4.search(itemdata)#汉字单位
+
+                if match1:
+                    yaowu = re.sub(pattern3, '', itemdata)
+                    weight = match1.group()
+                    if yaowu:
+                        medical.append(yaowu)
+                    medical.append(weight)
+                    if medical[-2] == 'None':
+                        medical.pop(-2)
+                elif match2:
+                    yaowu = re.sub(pattern1, '', itemdata)
+                    weight = match2.group()
+                    if yaowu:
+                        medical.append(yaowu)
+                    medical.append(weight)
+                    if medical[-2] == 'None':
+                        medical.pop(-2)
+                elif match3:
+                    yaowu = re.sub(pattern2, '', itemdata)
+                    weight = match3.group()
+                    if yaowu:
+                        medical.append(yaowu)
+                    medical.append(weight)
+                    if medical[-2] == 'None':
+                        medical.pop(-2)
+                elif match4:
+                    yaowu = re.sub(pattern4, '', itemdata)
+                    weight = match4.group()
+                    if yaowu:
+                        medical.append(yaowu)
+                    medical.append(weight)
+                    if medical[-2] == 'None':
+                        medical.pop(-2)
+                else:
+                    if itemdata:
+                        medical.append(itemdata)
+                        medical.append('None')
+
+                if check_ge:
+                    num=0
+                    for i in medical:
+                        if i=='None':
+                            medical[num]=weight
+                        num+=1
+            else:
+                medical.append(itemdata)
+            checkBH+=1
+            # print 'medical',medical
+        medicallist.append(medical)
+        medical=[]
+    finalmedicallist=[]
+    for content in medicallist:
+        num=0
+        for icontent in content:
+            if icontent.find('～')>-1:
+                content[num]=icontent[icontent.find('～')+1:]
+            if icontent.find('-') > -1:
+                content[num] = icontent[icontent.find('-') + 1:]
+            num+=1
+        finalmedicallist.append(content)
+    return finalmedicallist
+
+
+if __name__ == '__main__':
+    print 'dataFrom5w main...'
+    ######################  一、先把数据单独提出来 分别预处理
+    # readexcelname='../exceldata/5w_data.xlsx'
+    # csv_name='../formulaData_1/data5W.csv'
+
+    #step 1 把5w_data.xlsx表的全部内容存放在一个csv里(后续可略去
+    # sheet_index=0
+    # data_process.exceltocsv(readexcelname,sheet_index,csv_name)
+
+    # step 2 把功效和配伍单独提出来放在单独的csv里处理
+    # csvname1 = '../formulaData_1/pres_5W.csv'
+    # csvname2 = '../formulaData_1/func_5W.csv'
+    # pickData(csv_name,csvname1,csvname2)
+
+
+    ###################### 二、开始正式处理配伍数据
+    #step 1 处理文本中的空格逗号、分号等,切割
+    # readcsvname='../formulaData_1/pres_5W.csv'
+    # writecsvname='../formulaData_1/pres_5W_1.csv'
+    # dataDetailProcess.composition_process(readcsvname,writecsvname)
+
+    #step 2 处理用空格切割后还存在的多余的空格？       7-31 不需要这步
+    # readcsvname='../formulaData_1/pres_5W_1.csv'
+    # writecsvname='../formulaData_1/pres_5W_2.csv'
+    # dataDetailProcess.process_blank(readcsvname,writecsvname)
+
+    # step 3 去括号等一些附加说明
+    # (1)去掉‘各’字或括号内容（还有小部分扩号需人工去除
+    # readcsvname ='../formulaData_1/pres_5W_1.csv'
+    # datalist=dataDetailProcess.splitnumandstr(readcsvname)
+    # writecsvname='../formulaData_1/pres_5W_2.csv'
+    # data_process.write_in_csv(writecsvname, datalist)
+
+    # (2)删除没有内容的项  start（需重复做两~三次 处理,,,这种连着的情况
+    # readcsvname ='../formulaData_1/pres_5W_2.csv'
+    # writecsvname='../formulaData_1/pres_5W_3.csv'
+    # csvdata = data_process.read_csv(readcsvname)
+    # datas = []
+    # for item in csvdata:
+    #     num = 0
+    #     for itemdata in item:
+    #         if itemdata == '':
+    #             item.pop(num)
+    #         num += 1
+    #     datas.append(item)
+    # data_process.write_in_csv(writecsvname, datas)
+    #删除没有内容的项  end
+
+
+    # # (3)药名-数量单位 一一提取匹配；处理“各”的情况 补填；处理“等分”，“少许”等词清除
+    # readcsvname='../formulaData_1/pres_5W_3.csv'
+    # writecsvname='../formulaData_1/pres_5W_4.csv'
+    # finalmedicallist=extractnumfromstr(readcsvname, writecsvname)
+    # data_process.write_in_csv(writecsvname, finalmedicallist)
+
+    #8-1 目前考虑用one-hot表示，先不做这步
+    # #step 5 把单位kg,钱，两 统一为 g（克）
+    # csvname='../formulaData_1/pres_5W_4.csv'
+    # normalList= dataDetailProcess.unitTransformation(csvname)
+    # writecsvname='../formulaData_1/pres_5W_5.csv'
+    # data_process.write_in_csv(writecsvname, normalList)
+    #
+    # #step 6 清洗一些none单位的杂音 如‘等分，少许’
+    # csvname='../formulaData_1/pres_5W_5.csv'
+    # noneList= dataDetailProcess.noneStandard(csvname)
+    # writecsvname='../formulaData_1/pres_5W_6.csv'
+    # data_process.write_in_csv(writecsvname, noneList)
+
+
